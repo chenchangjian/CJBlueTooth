a25f4f3270a7e9b7adcad7c4aa5385bf8f67dee8
chenchangjian <ccj9@qq.com>
2015-12-15 13:20:22 +0800

    初始化框架代码: 1. 框架源码 2. 框架demo

---
+++ CJBlueToothExample/CJBlueToothDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CJBlueToothDemo
+//
+//  Created by ccj on 15/12/15.
+//  Copyright © 2015年 ccj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
