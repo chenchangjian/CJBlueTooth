14cd71bb98432d2bedf0a828fc309fdd5c410dda
陈长见 <ccj@ccjdeMacBook-Pro.local>
2015-12-15 13:22:01 +0800

    初始化框架代码
    初始化框架代码:
    1. 框架源码
    2. 框架demo

---
+++ CJBlueToothDemo/CJBlueToothDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CJBlueToothDemo
+//
+//  Created by ccj on 15/12/15.
+//  Copyright © 2015年 ccj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
